74e6055f1a580b8a47614c498e3114c0b8411c8d
Piero20181h057 <u20181h057@upc.edu.pe>
2023-06-04 19:24:04 -0500

    Add: Connect router films

--- src/app/core/home/home.component.ts
+++ src/app/core/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-  data = ['Angular', 'React', 'Vue'];
-  first_film = 'Angular';
-  second_film = 'React';
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json', //Solo acepta json
+    }),
+  };
+
+  
+  first_film : string ="";
+  second_film : string = "";
+  basePath = 'http://complex-final-lla.azurewebsites.net/';
+  data: Array<any> = [];
+  data2:Array<any> =[];
+  selectedFilm:any;
+
+
+
+  constructor(private http: HttpClient, private router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.getData().subscribe((data:any) =>{
+      
+      this.data = data;
+      this.data2 = data;
+      this.first_film = this.data[0];
+      this.second_film = this.data[1];
+      
+    });
+    
+  }
+
+  getData(){
+    return this.http.get(`${this.basePath}`);
+  }
+
+  change(){
+    this.data2 = this.data;
+    console.log(this.data2);
+    for (let i = 0; i < this.data2.length; i++) {
+      if (this.data2[i] === this.first_film) {
+        this.data2.splice(i,1);
+        console.log(this.data2);
+      }
+    }
+    this.first_film = this.first_film;
+  }
 }
