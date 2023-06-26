7858a1ff90e9670adb185f9f3f5448aa56fc63fc
lecav <lecav30@outlook.com>
2023-06-26 09:44:13 -0500

    Feat: Remove cicles in Dijkstra

--- src/app/core/chart/chart.component.ts
+++ src/app/core/chart/chart.component.ts
@@ -1,6 +1,7 @@
 import { GraphService } from './../../service/graph.service';
 import { Component, OnInit, Input } from '@angular/core';
 import { Edge, Node } from '@swimlane/ngx-graph/public_api';
+import { count } from 'd3';
 
 @Component({
   selector: 'app-chart',
@@ -41,6 +42,56 @@ export class ChartComponent implements OnInit {
             source: temp_links[i].source,
             target: temp_links[i].target,
           });
+        }
+      }
+      for (let i = 0; i < this.links.length; i++) {
+        var source = this.links[i].source;
+        var target = this.links[i].target;
+        var arrSource = [];
+        var arrTarget = [];
+        for (let j = i; j < this.links.length; j++) {
+          if (
+            this.links[j].source == source ||
+            this.links[j].target == source
+          ) {
+            arrSource.push(this.links[j]);
+          }
+          if (
+            this.links[j].source == target ||
+            this.links[j].target == target
+          ) {
+            arrTarget.push(this.links[j]);
+          }
+        }
+        if (arrSource.length == 3) {
+          console.log(arrSource);
+          for (let k = 0; k < arrSource.length; k++) {
+            if (
+              arrSource[k].source == this.films[0] ||
+              arrSource[k].source == this.films[this.films.length - 1] ||
+              arrSource[k].target == this.films[0] ||
+              arrSource[k].target == this.films[this.films.length - 1]
+            ) {
+              console.log(arrSource[k]);
+              var index = this.links.indexOf(arrSource[k]);
+              this.links.splice(index, 1);
+            }
+          }
+        }
+        if (arrTarget.length == 3) {
+          console.log(arrTarget);
+          for (let k = 0; k < arrTarget.length; k++) {
+            if (
+              arrTarget[k].source == this.films[0] ||
+              arrTarget[k].source == this.films[this.films.length - 1] ||
+              arrTarget[k].target == this.films[0] ||
+              arrTarget[k].target == this.films[this.films.length - 1]
+            ) {
+              console.log(arrTarget[k]);
+              var index = this.links.indexOf(arrTarget[k]);
+              this.links.splice(index, 1);
+            }
+          }
         }
       }
       this.mylinks = this.links;
